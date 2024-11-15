db28850e40294f51b0b8eb375471282577904ed1
yohanes1m <johnyemande@gmail.com>
2024-11-15 22:09:23 +0300

    row

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
     (async () => {
       try {
         const request = await axios.get(requests.fetchNetflixOriginals);
-        console.log(request);
+        // console.log(request);
         setMovie(
           request.data.results[
             Math.floor(Math.random() * request.data.results.length)
